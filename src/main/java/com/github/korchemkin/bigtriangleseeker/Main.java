d8c2100e9619fb434e1f74408c6ccbd1bc6a565a
korchemkin <dmitri.korchemkin@gmail.com>
2019-10-14 22:44:16 +0300

    init repo

---
+++ src/main/java/com/github/korchemkin/bigtriangleseeker/Main.java
@@ -0,0 +1,9 @@
+package com.github.korchemkin.bigtriangleseeker;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/main/java/com/github/korchemkin/bigtriangleseeker/Seeker.java
@@ -0,0 +1,5 @@
+package com.github.korchemkin.bigtriangleseeker;
+
+public class Seeker {
+
+}
