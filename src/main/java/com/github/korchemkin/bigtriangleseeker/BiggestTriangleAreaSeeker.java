f80bc4ebd70c344f24cb7ab98979b296306a7b15
Dmitri <korchemkin>
2019-10-18 00:45:50 +0300

    Refactoring

--- src/main/java/com/github/korchemkin/bigtriangleseeker/BiggestTriangleAreaSeeker.java
+++ src/main/java/com/github/korchemkin/bigtriangleseeker/BiggestTriangleAreaSeeker.java
@@ -9,50 +9,70 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+/**
+ *
+ * Finds the largest triangle area by coordinates in the input file.
+ */
 class BiggestTriangleAreaSeeker {
-	private final int rightCoordsLength = 6;
-	private double biggestTriangleArea;
-	private String desiredLine = "";
+    /**
+     * The largest triangle area in the input file.
+     */
+    private double biggestTriangleArea;
+    /**
+     * Searching results.
+     */
+    private String desiredLine = "";
 
-	private List<Integer> convertCoords(String[] coords) {
-		return Arrays.stream(coords)
-				.map(Integer::valueOf)
-				.collect(Collectors.toList());
-	}
+    private List<Integer> convertCoords(final String[] coords) {
+        return Arrays.stream(coords)
+            .map(Integer::valueOf)
+            .collect(Collectors.toList());
+    }
+    /**
+     *
+     * The coordinates of the first vertex of a triangle x1, y1.
+     * Second vertex x2, y2.
+     * Third vertex x3, y3.
+     * The calculation formula is ((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1)) / 2
+     * @param coords [x1, y1, x2, y2, x3, y3]
+     * @return triangle area
+     */
+    private double calcTriangleAreaByCoords(final List<Integer> coords) {
+        return (
+            (coords.get(2) - coords.get(0)) * (coords.get(5) - coords.get(1))
+            - (coords.get(4) - coords.get(0)) * (coords.get(3) - coords.get(1))
+        ) / 2.0;
+    }
 
-	private double squareByCoords(List<Integer> coords) {
-		return (
-				(coords.get(2) - coords.get(0)) * (coords.get(5) - coords.get(1))
-						- (coords.get(4) - coords.get(0)) * (coords.get(3) - coords.get(1))
-		) / 2.0;
-	}
+    private void calcLine(final String line) {
+        double triangleArea = this.calcTriangleAreaByCoords(
+                this.convertCoords(line.split(" "))
+        );
 
-	private void output(String outputFilePath) {
-		// TODO
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
-			writer.write(desiredLine);
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        if (triangleArea > this.biggestTriangleArea) {
+            this.biggestTriangleArea = triangleArea;
+            this.desiredLine = line;
+        }
+    }
 
-	void findAndOutput(String inputFilePath, String outputFilePath) {
-		// TODO
-		try (Stream<String> stream = Files.lines(Paths.get(inputFilePath))) {
-			stream.filter(x -> x.split(" ").length == this.rightCoordsLength)
-					.forEach(x -> {
-						double triangleArea = this.squareByCoords(this.convertCoords(x.split(" ")));
-						if (triangleArea > this.biggestTriangleArea) {
-							this.biggestTriangleArea = triangleArea;
-							this.desiredLine = x;
-						}
-					});
-			this.output(outputFilePath);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private void output(final String outputFilePath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath))) {
+            writer.write(desiredLine);
+        } catch (IOException e) {
+            // TODO
+            e.printStackTrace();
+        }
+    }
+
+    void findAndOutput(final String inputFilePath, final String outputFilePath) {
+        try (Stream<String> stream = Files.lines(Paths.get(inputFilePath))) {
+            final int rightCoordsLength = 6;
+            stream.filter(x -> x.split(" ").length == rightCoordsLength)
+                .forEach(this::calcLine);
+            this.output(outputFilePath);
+        } catch (IOException e) {
+            // TODO
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/github/korchemkin/bigtriangleseeker/Main.java
+++ src/main/java/com/github/korchemkin/bigtriangleseeker/Main.java
@@ -1,14 +1,21 @@
 package com.github.korchemkin.bigtriangleseeker;
 
-public class Main {
-	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.out.println("\n\nPlease, pass paths of input and output files. "
-					+ "For more info https://github.com/korchemkin/bigtriangleseeker\n\n");
-			return;
-		}
+public final class Main {
+    private Main() { }
+    /**
+     *
+     * @param args
+     * args[0] Should contain the path to the input file with coords of triangle (one line one triangle)
+     * args[1] Should contain the path to the output file to which the result is written
+     */
+    public static void main(final String[] args) {
+        if (args.length < 2) {
+            System.out.println("\n\nPlease, pass paths of input and output files. "
+                + "For more info https://github.com/korchemkin/bigtriangleseeker\n\n");
+            return;
+        }
 
-		BiggestTriangleAreaSeeker seeker = new BiggestTriangleAreaSeeker();
-		seeker.findAndOutput(args[0], args[1]);
-	}
+        BiggestTriangleAreaSeeker seeker = new BiggestTriangleAreaSeeker();
+        seeker.findAndOutput(args[0], args[1]);
+    }
 }
